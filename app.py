6dd2be4687605225ae19e017f420d546ddb39cfb
fun4wut <nihouze@163.com>
2018-12-28 19:45:26 +0800

    addw

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask
-
+import os
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+from models import initDB
 
 
 @app.route('/')
 def hello_world():
+    initDB()
     return 'Hello World!'
 
 
 if __name__ == '__main__':
     app.run()
+
---
+++ models.py
@@ -0,0 +1,30 @@
+import os
+from flask_sqlalchemy import SQLAlchemy
+from app import app
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://ISEAT_DEV:password@101.132.135.239:3306/ISEAT_DEV'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+class Seat(db.Model):
+    __tablename__ = 'seats'
+    id = db.Column(db.String(255),nullable=False,primary_key=True,autoincrement=False)
+    library = db.Column(db.String(255),nullable=False,primary_key=False,autoincrement=False)
+    floor = db.Column(db.Integer,nullable=False,primary_key=False,autoincrement=False)
+    room = db.Column(db.String(255),nullable=False,primary_key=False,autoincrement=False)
+    occupied = db.Column(db.String(255),nullable=True,primary_key=False,autoincrement=False)
+    messages = db.Column(db.String(255),nullable=True,primary_key=False,autoincrement=False)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.String(255), nullable=False, primary_key=True, autoincrement=False)
+    name = db.Column(db.String(255), nullable=False, primary_key=False, autoincrement=False)
+    violation = db.Column(db.Integer, nullable=False, primary_key=False, autoincrement=False)
+    study_time = db.Column(db.String(255), nullable=False, primary_key=False, autoincrement=False)
+    message = db.Column(db.String(255), nullable=False, primary_key=True, autoincrement=False)
+    previous_seat = db.Column(db.String(255), nullable=False, primary_key=True, autoincrement=False)
+    present_seat = db.Column(db.String(255), nullable=False, primary_key=True, autoincrement=False)
+
+def initDB():
+    db.create_all()
