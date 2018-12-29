1be31d2a3cde31e78ff3596896042372fa36122a
fun4wut <nihouze@163.com>
2018-12-29 12:11:31 +0800

    addw

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 from flask import Flask
 import os
-from flask_sqlalchemy import SQLAlchemy
+import json
 app = Flask(__name__)
-from models import initDB
+
+from models import initDB,User,Seat
 
 
-@app.route('/')
-def hello_world():
-    initDB()
-    return 'Hello World!'
+@app.route('/wx/floor/<n>',methods=['GET'])
+def hello_world(n):
+    img_url = "https://gss0.baidu.com/94o3dSag_xI4khGko9WTAnF6hhy/zhidao/pic/item/37d12f2eb9389b50579fb4a38e35e5dde6116eda.jpg"
+    seats = Seat.query.filter_by(floor=n).all()
+    name = set([x.room for x in seats])
+    img = [img_url for x in name]
+    rest = []
+    current = []
+    for x in name:
+        tmp_seat = Seat.query.filter_by(floor=n,room=x).all()
+        tmp_num = len(tmp_seat)
+        tmp_rest = 0
+        for y in tmp_seat:
+            if y.occupied is None: tmp_rest+=1
+        rest.append(tmp_rest)
+        current.append(tmp_num-tmp_rest)
+
+    return json.dumps({"img":img,"name":list(name),"rest":rest,"current":current},ensure_ascii=False)
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -2,18 +2,19 @@ import os
 from flask_sqlalchemy import SQLAlchemy
 from app import app
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://ISEAT_DEV:password@101.132.135.239:3306/ISEAT_DEV'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@101.132.135.239:3306/ISEAT_DEV?charset=utf8'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
 class Seat(db.Model):
     __tablename__ = 'seats'
-    id = db.Column(db.String(255),nullable=False,primary_key=True,autoincrement=False)
+    id = db.Column(db.String(255),nullable=False,primary_key=True,autoincrement=True)
     library = db.Column(db.String(255),nullable=False,primary_key=False,autoincrement=False)
     floor = db.Column(db.Integer,nullable=False,primary_key=False,autoincrement=False)
     room = db.Column(db.String(255),nullable=False,primary_key=False,autoincrement=False)
     occupied = db.Column(db.String(255),nullable=True,primary_key=False,autoincrement=False)
     messages = db.Column(db.String(255),nullable=True,primary_key=False,autoincrement=False)
+
 
 
 class User(db.Model):
@@ -25,6 +26,8 @@ class User(db.Model):
     message = db.Column(db.String(255), nullable=False, primary_key=True, autoincrement=False)
     previous_seat = db.Column(db.String(255), nullable=False, primary_key=True, autoincrement=False)
     present_seat = db.Column(db.String(255), nullable=False, primary_key=True, autoincrement=False)
+
+
 
 def initDB():
     db.create_all()
