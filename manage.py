b9c18bca76426b64df634a4d127802e20f60d42a
Humpy Lin <humpylin@gmail.com>
2019-02-24 19:54:31 +0800

    增加了 flask-migration

--- manage.py
+++ manage.py
@@ -1,5 +1,11 @@
 import os
 from app import db, create_app
 from app.models import User, Seat
+from flask_migrate import Migrate
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+migrate = Migrate(app, db)
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User)
