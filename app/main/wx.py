1b6c67f85c0668e35c82e625f5f268f3db1a6387
fun4wut <nihouze@163.com>
2019-02-24 19:56:29 +0800

    修改字段

--- app/main/wx.py
+++ app/main/wx.py
@@ -1,12 +1,12 @@
 import json
 from .. import db
-from ..models import User, Seat
+from ..models import User, Seat, Area
 from . import main
 
 @main.route('/wx/floor/<n>', methods=['GET'])
 def overviewOfFloor(n):
     img_url = "https://gss0.baidu.com/94o3dSag_xI4khGko9WTAnF6hhy/zhidao/pic/item/37d12f2eb9389b50579fb4a38e35e5dde6116eda.jpg"
-    seats = Seat.query.filter_by(floor=n).all()
+    areas = Area.query.filter_by(floor=n).all()
     name = set([x.room for x in seats])
     num = len(name)
     img = [img_url for x in name]
@@ -14,7 +14,7 @@ def overviewOfFloor(n):
     current = []
     total = []
     for x in name:
-        tmp_seat = Seat.query.filter_by(floor=n,room=x).all()
+        tmp_seat = Area.query.filter_by(floor=n,room=x).all()
         tmp_num = len(tmp_seat)
         tmp_rest = 0
         for y in tmp_seat:
@@ -24,3 +24,7 @@ def overviewOfFloor(n):
         total.append(tmp_num)
 
     return json.dumps({"img":img,"name":list(name),"rest":rest,"current":current,"num":num,"total":total},ensure_ascii=False)
+
+@main.route('/wx/user/<id>',methods=['GET'])
+def overviewOfUser(id):
+    pass
--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from . import db
 
-class Seat(db.Model):
-    __tablename__ = 'seats'
+class Area(db.Model):
+    __tablename__ = 'areas'
     id = db.Column(db.String(255),nullable=False,primary_key=True,autoincrement=True)
     library = db.Column(db.String(255),nullable=False,primary_key=False,autoincrement=False)
     floor = db.Column(db.Integer,nullable=False,primary_key=False,autoincrement=False)
@@ -15,9 +15,21 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.String(255), nullable=False, primary_key=True, autoincrement=False)
     name = db.Column(db.String(255), nullable=False, primary_key=False, autoincrement=False)
-    violation = db.Column(db.Integer, nullable=False, primary_key=False, autoincrement=False)
+    credit = db.Column(db.Integer, nullable=False, primary_key=False, autoincrement=False)
     study_time = db.Column(db.String(255), nullable=False, primary_key=False, autoincrement=False)
-    message = db.Column(db.String(255), nullable=False, primary_key=True, autoincrement=False)
-    previous_seat = db.Column(db.String(255), nullable=False, primary_key=True, autoincrement=False)
-    present_seat = db.Column(db.String(255), nullable=False, primary_key=True, autoincrement=False)
-
+    message = db.Column(db.String(255), nullable=False, primary_key=False, autoincrement=False)
+    previous_seat = db.Column(db.String(255), nullable=False, primary_key=False, autoincrement=False)
+    present_seat = db.Column(db.String(255), nullable=False, primary_key=False, autoincrement=False)
+    condition = db.Column(db.Integer,nullable=False,primary_key=False,autoincrement=False)
+    start_time = db.Column(db.Time,nullable=True,primary_key=False,autoincrement=False)
+    passwd = db.Column(db.String(255),nullable=True,primary_key=False,autoincrement=False)
+class Table(db.Model):
+    __tablename__ = 'tables'
+    id = db.Column(db.String(255),nullable=False,primary_key=True,autoincrement=False)
+    name = db.Column(db.String(255),nullable=False,primary_key=True,autoincrement=False)
+    type = db.Column(db.String(255),nullable=False,primary_key=True,autoincrement=False)
+    ch1 = db.Column(db.String(255),nullable=True,primary_key=True,autoincrement=False)
+    ch2 = db.Column(db.String(255),nullable=True,primary_key=True,autoincrement=False)
+    ch3 = db.Column(db.String(255),nullable=True,primary_key=True,autoincrement=False)
+    ch4 = db.Column(db.String(255),nullable=True,primary_key=True,autoincrement=False)
+    
