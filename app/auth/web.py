0f84159fcb079e84fb049daea10a301d7ef10a1d
Humpy Lin <humpylin@gmail.com>
2019-02-24 19:37:20 +0800

    增加了登陆时候的表单处理

--- app/auth/web.py
+++ app/auth/web.py
@@ -1,20 +1,18 @@
 from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user
 from . import auth
-from ..models import User 
-from .forms import LoginForm
+from ..models import User
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        print(1)
-        if user is not None and user.verify_password(form.password.data):
-            login_user(user, form.remember_me.data)
-            next = request.args.get('next')
-            if next is None or not next.startswith('/'):
-                next = url_for('main.index')
-            return redirect(next)
-        flash('Invalid username or password')
-    return render_template('login.html', form=form)
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(id=username).first()
+        if user is not None and user.verify_password(password):
+            login_user(user)
+            return redirect('main.index')
+        else:
+            return render_template('login.html')
+    elif request.method == 'GET':
+        return render_template('login.html')
